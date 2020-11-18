73ee9199bdc25ab7b28a93ecdbad2b2d89de19ff
Yap Zi Ming <zy078@live.mdx.ac.uk>
2020-11-18 13:28:40 +0000

    error fixed

---
+++ lessons.js
@@ -0,0 +1,83 @@
+let lessons=[
+    {
+        subject:"English",
+        location:"London",
+        price :130,
+        spaces:5,
+        imagePath:"images/english.png"
+
+    },
+    {
+        subject:"Math",
+        location:"Hendon",
+        price :120,
+        spaces:5,
+        imagePath:"images/math.png"
+
+    },
+    {
+        subject:"Music",
+        location:"Wembley",
+        price :200,
+        spaces:5,
+        imagePath:"images/music.png"
+
+    },
+    {
+        subject:"Science",
+        location:"Manchester",
+        price :150,
+        spaces:5,
+        imagePath:"images/science.png"
+
+    },
+    {
+        subject:"Geography",
+        location:"Liverpool",
+        price :80,
+        spaces:5,
+        imagePath:"images/geography.png"
+
+    },
+    {
+        subject:"Chemistry",
+        location:"Sunderland",
+        price :230,
+        spaces:5,
+        imagePath:"images/chemistry.png"
+
+    },
+   {
+        subject:"Physics",
+        location:"Watford",
+        price :220,
+        spaces:5,
+        imagePath:"images/physics.png"
+
+    },
+    {
+        subject:"Biology",
+        location:"Reading",
+        price :250,
+        spaces:5,
+        imagePath:"images/biology.png"
+
+    },
+    {
+        subject:"Account",
+        location:"Oxford",
+        price :100,
+        spaces:5,
+        imagePath:"images/account.png"
+
+    },
+    {
+        subject:"Swimming",
+        location:"Swindon",
+        price :200,
+        spaces:5,
+        imagePath:"images/swimming.png"
+
+    }
+]
+
